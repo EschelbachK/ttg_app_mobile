cf2051267adc8dd9611569e5bde603d11b4eff4d
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-26 17:23:55 +0100

    refactor: clean main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,62 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TTG App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'TrainToGain'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  Future<void> testLogin() async {
-    final url = Uri.parse('http://10.0.2.2:8080/api/auth/login');
-
-    final response = await http.post(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'email': 'test@test.de',
-        'password': '123456'
-      }),
-    );
-
-    print('Status: ${response.statusCode}');
-    print('Body: ${response.body}');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text(
-          'Drücke den Button für API Test',
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: testLogin,
-        tooltip: 'Login Test',
-        child: const Icon(Icons.login),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
