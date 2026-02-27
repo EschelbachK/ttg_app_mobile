af9a865c4d118e26ae870323353b9ad799eda9f2
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-27 16:16:50 +0100

    feat: fix

--- lib/core/network/dio_provider.dart
+++ lib/core/network/dio_provider.dart
@@ -6,7 +6,7 @@ import 'auth_interceptor.dart';
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
     BaseOptions(
-      baseUrl: 'http://localhost:8080/api',
+      baseUrl: 'http://10.0.2.2:8080/api',
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
     ),
