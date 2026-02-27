75d6b3755bfe427b361405cb6e47f93d71206463
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-27 12:39:20 +0100

    feat: add global bottom navigation scaffold

---
+++ lib/core/navigation/main_navigation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MainNavigation extends StatelessWidget {
+  final Widget child;
+
+  const MainNavigation({super.key, required this.child});
+
+  int _getIndex(BuildContext context) {
+    final location = GoRouterState.of(context).matchedLocation;
+
+    if (location.startsWith('/dashboard')) return 0;
+
+    return 0;
+  }
+
+  void _onTap(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/dashboard');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentIndex = _getIndex(context);
+
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) => _onTap(context, index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: "Dashboard",
+          ),
+        ],
+      ),
+    );
+  }
+}
