287b3929bcc1de096740c49586309a282710a25e
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-27 12:33:09 +0100

    feat: integrate global dark wine theme into MaterialApp.router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
 
 void main() {
   runApp(
@@ -19,6 +21,7 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme,
       routerConfig: router,
     );
   }
