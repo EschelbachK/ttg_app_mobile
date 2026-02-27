58baffc6c948504a869ac094e576a58b0e1f9762
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-27 14:02:20 +0100

    feat(workout): add TrainingFolder model

---
+++ lib/features/models/training_folder.dart
@@ -0,0 +1,35 @@
+import 'training_exercise.dart';
+
+class TrainingFolder {
+  final String id;
+  final String name;
+  final String planId;
+  final List<TrainingExercise> exercises;
+
+  TrainingFolder({
+    required this.id,
+    required this.name,
+    required this.planId,
+    required this.exercises,
+  });
+
+  factory TrainingFolder.fromJson(Map<String, dynamic> json) {
+    return TrainingFolder(
+      id: json['id'],
+      name: json['name'],
+      planId: json['planId'],
+      exercises: (json['exercises'] as List<dynamic>)
+          .map((e) => TrainingExercise.fromJson(e))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'planId': planId,
+      'exercises': exercises.map((e) => e.toJson()).toList(),
+    };
+  }
+}
