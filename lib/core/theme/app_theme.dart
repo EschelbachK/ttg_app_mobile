84b93f2653d66bc832b9547ed0fa43c981d0fddd
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-08 10:37:47 +0200

    style: clean app_theme structure

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const primaryRed = Color(0xFFE10600);
 
-  static const Color primaryRed = Color(0xFFE10600);
-
-  static const LinearGradient primaryButtonGradient = LinearGradient(
-    colors: [
-      Color(0xFFFF1A1A),
-      Color(0xFFE10600),
-    ],
+  static const primaryButtonGradient = LinearGradient(
+    colors: [Color(0xFFFF1A1A), Color(0xFFE10600)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
-  static const Color background = Color(0xFF000000);
-  static const Color card = Color(0xFF1A1F26);
+  static const background = Color(0xFF000000);
+  static const card = Color(0xFF1A1F26);
 
-  static ThemeData darkTheme = ThemeData.dark().copyWith(
+  static final darkTheme = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: background,
     primaryColor: primaryRed,
   );
