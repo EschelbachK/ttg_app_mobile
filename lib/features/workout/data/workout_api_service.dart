f40e68c2de94ae6a70c064c4d280a43c1dda96ff
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-08 10:56:45 +0200

    fix: handle api response map/list mismatch in workout service

--- lib/features/workout/data/workout_api_service.dart
+++ lib/features/workout/data/workout_api_service.dart
@@ -9,51 +9,39 @@ class WorkoutApiService {
 
   WorkoutApiService(this._dio);
 
+  List<dynamic> _extract(dynamic data) {
+    if (data is List) return data;
+    if (data is Map && data['content'] is List) {
+      return List<dynamic>.from(data['content']);
+    }
+    if (data is Map && data['data'] is List) {
+      return List<dynamic>.from(data['data']);
+    }
+    return [];
+  }
+
   Future<List<T>> _mapList<T>(
       String path,
-      T Function(Map<String, dynamic>) fromJson) async {
+      T Function(Map<String, dynamic>) fromJson,
+      ) async {
     final res = await _dio.get(path);
-    final data = res.data as List<dynamic>;
-    return data.map((e) => fromJson(e)).toList();
+    final list = _extract(res.data);
+    return list.map((e) => fromJson(e)).toList();
   }
 
   Future<List<TrainingPlan>> getTrainingPlans() =>
       _mapList('/training-plans', TrainingPlan.fromJson);
 
-  Future<TrainingPlan> createTrainingPlan(String name) async {
-    final res =
-    await _dio.post('/training-plans', data: {'name': name});
-    return TrainingPlan.fromJson(res.data);
-  }
-
-  Future<void> deleteTrainingPlan(String id) =>
-      _dio.delete('/training-plans/$id');
-
   Future<List<TrainingFolder>> getFolders(String planId) =>
-      _mapList(
-          '/training-plans/$planId/folders',
+      _mapList('/training-plans/$planId/folders',
           TrainingFolder.fromJson);
 
-  Future<TrainingFolder> createFolder(
-      String planId, String name) async {
-    final res = await _dio.post(
-      '/training-plans/$planId/folders',
-      data: {'name': name},
-    );
-    return TrainingFolder.fromJson(res.data);
-  }
-
-  Future<void> deleteFolder(String planId, String folderId) =>
-      _dio.delete('/training-plans/$planId/folders/$folderId');
-
   Future<List<TrainingExercise>> getExercises(String folderId) =>
-      _mapList(
-          '/folders/$folderId/exercises',
+      _mapList('/folders/$folderId/exercises',
           TrainingExercise.fromJson);
 
   Future<List<SetEntry>> getSets(String exerciseId) =>
-      _mapList(
-          '/exercises/$exerciseId/sets',
+      _mapList('/exercises/$exerciseId/sets',
           SetEntry.fromJson);
 
   Future<SetEntry> updateSet(
@@ -61,7 +49,8 @@ class WorkoutApiService {
       String setId,
       int reps,
       double weight,
-      bool completed) async {
+      bool completed,
+      ) async {
     final res = await _dio.put(
       '/exercises/$exerciseId/sets/$setId',
       data: {
