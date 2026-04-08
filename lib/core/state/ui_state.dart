5a8ad19b327ff4b8460827e3375a12c1c36fdcad
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-08 10:37:37 +0200

    refactor: optimize ui_state constructors

--- lib/core/state/ui_state.dart
+++ lib/core/state/ui_state.dart
@@ -3,18 +3,18 @@ class UiState<T> {
   final T? data;
   final String? error;
 
-  const UiState({
+  const UiState._({
     required this.isLoading,
     this.data,
     this.error,
   });
 
-  factory UiState.initial() => const UiState(isLoading: false);
+  const UiState.initial() : this._(isLoading: false);
 
-  factory UiState.loading() => const UiState(isLoading: true);
+  const UiState.loading() : this._(isLoading: true);
 
-  factory UiState.success(T data) => UiState(isLoading: false, data: data);
+  const UiState.success(T data) : this._(isLoading: false, data: data);
 
-  factory UiState.error(String message) =>
-      UiState(isLoading: false, error: message);
+  const UiState.error(String message)
+      : this._(isLoading: false, error: message);
 }
