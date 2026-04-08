6ddd50dfd9b59bfcdb0ecd9b1650d1653da97bd7
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-08 10:38:55 +0200

    refactor: clean catalog provider

--- lib/features/catalog/state/exercise_catalog_provider.dart
+++ lib/features/catalog/state/exercise_catalog_provider.dart
@@ -2,18 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../api/catalog_api.dart';
 import '../models/exercise_catalog_item.dart';
 
-final catalogApiProvider = Provider<CatalogApi>((ref) {
-  return CatalogApi();
-});
+final catalogApiProvider = Provider((ref) => CatalogApi());
 
 final exerciseCatalogProvider =
 FutureProvider<List<ExerciseCatalogItem>>((ref) async {
-
-  final api = ref.read(catalogApiProvider);
-
-  final exercises = await api.fetchExercises();
-
-  return exercises
-      .map((e) => ExerciseCatalogItem.fromJson(e))
-      .toList();
+  final data = await ref.read(catalogApiProvider).fetchExercises();
+  return data.map((e) => ExerciseCatalogItem.fromJson(e)).toList();
 });
