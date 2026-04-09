972ed58150cafd3a1d33f83944aa1b56e18ccfe6
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-09 12:40:46 +0200

    Fix MotivationNotifier.checkPRs to use List<String> from MotivationEngine

--- lib/features/workout/providers/motivation_provider.dart
+++ lib/features/workout/providers/motivation_provider.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../application/motivation_engine.dart';
 import '../domain/workout_session.dart';
+import '../domain/workout_training_plan.dart';
 
-final motivationProvider = ChangeNotifierProvider<MotivationNotifier>((ref) => MotivationNotifier());
+final motivationProvider =
+ChangeNotifierProvider<MotivationNotifier>((ref) => MotivationNotifier());
 
 class MotivationNotifier extends ChangeNotifier {
   final MotivationEngine engine = MotivationEngine();
 
+  void Function(int)? onStreak;
+  void Function(List<String>)? onPR;
+
   void updateStreakFromSession(WorkoutSession session) {
+    final oldStreak = engine.streak.streakCount;
     engine.updateStreak(session);
+    if (engine.streak.streakCount > oldStreak) {
+      onStreak?.call(engine.streak.streakCount);
+    }
     notifyListeners();
   }
 
   void checkPRs(String exerciseId, List<ExerciseSession> exercises) {
-    engine.checkPRs(exerciseId, exercises);
+    final List<String> newPRs = engine.checkPRs(exerciseId, exercises);
+    if (newPRs.isNotEmpty) {
+      onPR?.call(newPRs);
+    }
     notifyListeners();
   }
 }
