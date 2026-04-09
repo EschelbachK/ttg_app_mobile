438f1a0d2326b5a3336c3c476fafa33c70d107ba
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-09 12:53:49 +0200

    Fix workoutProvider initialization with MotivationNotifier

--- lib/features/workout/providers/workout_provider.dart
+++ lib/features/workout/providers/workout_provider.dart
@@ -2,13 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../application/workout_controller.dart';
 import '../application/workout_state.dart';
 import '../data/workout_api_service.dart';
+import '../providers/motivation_provider.dart';
 
 final workoutApiProvider =
 Provider<WorkoutApiService>((ref) => ref.read(workoutApiServiceProvider));
 
 final workoutProvider =
 StateNotifierProvider<WorkoutController, WorkoutState>((ref) {
-  final controller = WorkoutController(ref.read(workoutApiProvider));
+  final api = ref.read(workoutApiProvider);
+  final motivator = ref.read(motivationProvider.notifier);
+  final controller = WorkoutController(api, motivator);
   Future.microtask(controller.init);
   return controller;
 });
