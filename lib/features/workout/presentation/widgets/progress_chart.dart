c9bdb14763d4050bca456ae38a53b36e64a0b75f
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-09 12:01:40 +0200

    improve progress chart structure and performance

--- lib/features/workout/presentation/widgets/progress_chart.dart
+++ lib/features/workout/presentation/widgets/progress_chart.dart
@@ -12,49 +12,62 @@ class ProgressChart extends StatelessWidget {
   Widget build(BuildContext context) {
     if (history.isEmpty) return const SizedBox.shrink();
 
-    final weightSpots = <FlSpot>[];
-    final repsSpots = <FlSpot>[];
-    final volumeSpots = <FlSpot>[];
-
-    for (var i = 0; i < history.length; i++) {
-      final h = history[i];
-
-      weightSpots.add(FlSpot(i.toDouble(), h.weight));
-      repsSpots.add(FlSpot(i.toDouble(), h.reps.toDouble()));
-      volumeSpots.add(FlSpot(i.toDouble(), h.weight * h.reps));
-    }
+    final spots = _buildSpots();
 
     return TtgGlassCard(
       child: SizedBox(
-        height: 240,
+        height: 260,
         child: LineChart(
           LineChartData(
+            minX: 0,
+            maxX: (history.length - 1).toDouble(),
             gridData: FlGridData(show: false),
-            titlesData: FlTitlesData(show: false),
             borderData: FlBorderData(show: false),
+            titlesData: FlTitlesData(show: false),
+            lineTouchData: LineTouchData(enabled: true),
             lineBarsData: [
-              LineChartBarData(
-                spots: weightSpots,
-                isCurved: true,
-                dotData: FlDotData(show: false),
-                color: Colors.blue,
-              ),
-              LineChartBarData(
-                spots: repsSpots,
-                isCurved: true,
-                dotData: FlDotData(show: false),
-                color: Colors.green,
-              ),
-              LineChartBarData(
-                spots: volumeSpots,
-                isCurved: true,
-                dotData: FlDotData(show: false),
-                color: Colors.orange,
-              ),
+              _bar(spots.weight, Colors.blue),
+              _bar(spots.reps, Colors.green),
+              _bar(spots.volume, Colors.orange),
             ],
           ),
         ),
       ),
     );
   }
+
+  _ChartSpots _buildSpots() {
+    final weight = <FlSpot>[];
+    final reps = <FlSpot>[];
+    final volume = <FlSpot>[];
+
+    for (var i = 0; i < history.length; i++) {
+      final h = history[i];
+      final x = i.toDouble();
+
+      weight.add(FlSpot(x, h.weight));
+      reps.add(FlSpot(x, h.reps.toDouble()));
+      volume.add(FlSpot(x, h.weight * h.reps));
+    }
+
+    return _ChartSpots(weight, reps, volume);
+  }
+
+  LineChartBarData _bar(List<FlSpot> spots, Color color) {
+    return LineChartBarData(
+      spots: spots,
+      isCurved: true,
+      barWidth: 2.5,
+      dotData: FlDotData(show: false),
+      color: color,
+    );
+  }
+}
+
+class _ChartSpots {
+  final List<FlSpot> weight;
+  final List<FlSpot> reps;
+  final List<FlSpot> volume;
+
+  _ChartSpots(this.weight, this.reps, this.volume);
 }
