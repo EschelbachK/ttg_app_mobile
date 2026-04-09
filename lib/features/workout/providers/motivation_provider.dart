0b31cb550981edfe8037005db0c53767a0145170
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-09 13:38:48 +0200

    add haptic feedback based on motivation intensity

--- lib/features/workout/providers/motivation_provider.dart
+++ lib/features/workout/providers/motivation_provider.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../application/motivation_engine.dart';
 import '../application/motivation_state.dart';
 import '../domain/motivation/motivation_event.dart';
+import '../domain/motivation/motivation_level.dart';
 import '../domain/workout_session.dart';
 
 final motivationProvider =
@@ -27,6 +29,8 @@ class MotivationNotifier extends ChangeNotifier {
 
     _timer?.cancel();
 
+    _triggerHaptic(result.level);
+
     state = state.copyWith(
       last: result,
       visible: true,
@@ -39,5 +43,20 @@ class MotivationNotifier extends ChangeNotifier {
       state = state.copyWith(visible: false);
       notifyListeners();
     });
+  }
+
+  void _triggerHaptic(MotivationLevel level) {
+    switch (level) {
+      case MotivationLevel.low:
+        HapticFeedback.lightImpact();
+        break;
+      case MotivationLevel.medium:
+        HapticFeedback.mediumImpact();
+        break;
+      case MotivationLevel.high:
+      case MotivationLevel.extreme:
+        HapticFeedback.heavyImpact();
+        break;
+    }
   }
 }
