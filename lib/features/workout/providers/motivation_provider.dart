f6e8ac42e566f7b8f82189da2f28155bcf7c5b90
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-09 12:33:40 +0200

    Add MotivationNotifier with live updates

--- lib/features/workout/providers/motivation_provider.dart
+++ lib/features/workout/providers/motivation_provider.dart
@@ -1,4 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../application/motivation_engine.dart';
+import '../domain/workout_session.dart';
 
-final motivationProvider = Provider((ref) => MotivationEngine());
+final motivationProvider = ChangeNotifierProvider<MotivationNotifier>((ref) => MotivationNotifier());
+
+class MotivationNotifier extends ChangeNotifier {
+  final MotivationEngine engine = MotivationEngine();
+
+  void updateStreakFromSession(WorkoutSession session) {
+    engine.updateStreak(session);
+    notifyListeners();
+  }
+
+  void checkPRs(String exerciseId, List<ExerciseSession> exercises) {
+    engine.checkPRs(exerciseId, exercises);
+    notifyListeners();
+  }
+}
