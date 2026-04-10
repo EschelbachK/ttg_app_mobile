0600dce770b69b72200819baf1b53fba288fc540
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-10 16:39:32 +0200

    remove auto redirect from workout active wrapper

---
+++ lib/features/workout/presentation/screens/workout_active_wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'workout_active_screen.dart';
+
+class WorkoutActiveWrapper extends StatelessWidget {
+  const WorkoutActiveWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WorkoutActiveScreen();
+  }
+}
