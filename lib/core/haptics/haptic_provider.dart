a69bfba53fcf3050514ca9926be398da887a8ddb
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-20 11:59:24 +0200

    optimize haptic provider and remove unnecessary rebuilds

---
+++ lib/core/haptics/haptic_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../features/settings/application/settings_provider.dart';
+import 'haptic_service.dart';
+
+final hapticProvider = Provider<HapticController>((ref) {
+  final enabled = ref.read(settingsProvider).soundEnabled;
+  return HapticController(enabled);
+});
+
+class HapticController {
+  final bool enabled;
+
+  const HapticController(this.enabled);
+
+  void _run(void Function() fn) {
+    if (enabled) fn();
+  }
+
+  void light() => _run(HapticService.light);
+  void medium() => _run(HapticService.medium);
+  void heavy() => _run(HapticService.heavy);
+  void success() => _run(HapticService.success);
+}
