53a1bd03ca11ed3bbe1f9fe05a60a519ab625ac3
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-20 16:06:03 +0200

    optimize api client and prepare for offline handling

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -1,24 +1,36 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../features/settings/application/settings_provider.dart';
 import 'dio_provider.dart';
 
 final apiClientProvider = Provider<ApiClient>((ref) {
-  return ApiClient(ref.read(dioProvider));
+  return ApiClient(ref, ref.read(dioProvider));
 });
 
 class ApiClient {
+  final Ref ref;
   final Dio dio;
 
-  ApiClient(this.dio);
+  ApiClient(this.ref, this.dio);
+
+  bool get _offline => ref.read(settingsProvider).offlineMode;
+
+  Future<Response> _run(Future<Response> Function() req) async {
+    if (_offline) {
+      throw Exception('Offline mode active');
+    }
+    return await req();
+  }
 
   Future<Response> get(String path, {Map<String, dynamic>? query}) =>
-      dio.get(path, queryParameters: query);
+      _run(() => dio.get(path, queryParameters: query));
 
   Future<Response> post(String path, {Map<String, dynamic>? data}) =>
-      dio.post(path, data: data);
+      _run(() => dio.post(path, data: data));
 
   Future<Response> put(String path, {Map<String, dynamic>? data}) =>
-      dio.put(path, data: data);
+      _run(() => dio.put(path, data: data));
 
-  Future<Response> delete(String path) => dio.delete(path);
+  Future<Response> delete(String path) =>
+      _run(() => dio.delete(path));
 }
