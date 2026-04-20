0aa6cc4cf0aee24a092f51c242a191bee7025079
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-20 11:58:29 +0200

    optimize sound listener and reduce redundant settings reads

---
+++ lib/features/workout/providers/sound_listener.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/events/event_bus_provider.dart';
+import '../../../core/events/workout_events.dart';
+import '../../../core/audio/sound_provider.dart';
+import '../../../core/haptics/haptic_provider.dart';
+import '../../settings/application/settings_provider.dart';
+
+final soundListenerProvider = Provider((ref) {
+  final bus = ref.read(eventBusProvider);
+  final sound = ref.read(soundProvider);
+  final haptic = ref.read(hapticProvider);
+
+  void handle(bool enabled, void Function() action) {
+    if (enabled) action();
+  }
+
+  bus.on<TimerTickEvent>().listen((e) {
+    final enabled = ref.read(settingsProvider).soundEnabled;
+    if (e.seconds <= 3 && e.seconds > 0) {
+      handle(enabled, () {
+        sound.playBeep();
+        haptic.light();
+      });
+    }
+  });
+
+  bus.on<RestFinishedEvent>().listen((_) {
+    handle(ref.read(settingsProvider).soundEnabled, () {
+      sound.playFinish();
+      haptic.heavy();
+    });
+  });
+});
