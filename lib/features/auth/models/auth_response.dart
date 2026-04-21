5270d8af74de8f93e2c77fdf5a58c5ff1bff33e4
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-21 08:32:58 +0200

    extend auth response to include user

--- lib/features/auth/models/auth_response.dart
+++ lib/features/auth/models/auth_response.dart
@@ -1,41 +1,23 @@
+import 'user_model.dart';
+
 class AuthResponse {
   final String accessToken;
   final String refreshToken;
+  final UserModel user;
 
   AuthResponse({
     required this.accessToken,
     required this.refreshToken,
+    required this.user,
   });
 
   factory AuthResponse.fromJson(Map<String, dynamic> json) {
-
-    final Map<String, dynamic> data =
-    json.containsKey("data")
-        ? json["data"]
-        : json;
-
-    final accessToken =
-        data["accessToken"] ??
-            data["access_token"];
-
-    final refreshToken =
-        data["refreshToken"] ??
-            data["refresh_token"];
-
-    if (accessToken == null || refreshToken == null) {
-      throw Exception("Invalid auth response format: $json");
-    }
+    final data = json.containsKey('data') ? json['data'] : json;
 
     return AuthResponse(
-      accessToken: accessToken,
-      refreshToken: refreshToken,
+      accessToken: data['accessToken'],
+      refreshToken: data['refreshToken'],
+      user: UserModel.fromJson(data['user']),
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "accessToken": accessToken,
-      "refreshToken": refreshToken,
-    };
   }
 }
