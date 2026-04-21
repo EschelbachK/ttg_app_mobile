aef8408d73dd06091eeef714d27021fbc331171b
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-21 08:58:31 +0200

    cleanup dio provider

--- lib/core/network/dio_provider.dart
+++ lib/core/network/dio_provider.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../features/settings/application/settings_provider.dart';
+import '../settings/settings_controller.dart';
 import 'auth_interceptor.dart';
 
 final dioProvider = Provider<Dio>((ref) {
