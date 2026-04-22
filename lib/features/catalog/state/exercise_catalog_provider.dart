578c1c79d2b2158bd4e9a99e532693f35a23d45a
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-22 11:30:51 +0200

    update exercise catalog provider

--- lib/features/catalog/state/exercise_catalog_provider.dart
+++ lib/features/catalog/state/exercise_catalog_provider.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/network/dio_provider.dart';
 import '../api/catalog_api.dart';
 import '../models/exercise_catalog_item.dart';
 
-final catalogApiProvider = Provider((ref) => CatalogApi());
+final catalogApiProvider = Provider(
+      (ref) => CatalogApi(ref.read(dioProvider)),
+);
 
 final exerciseCatalogProvider =
 FutureProvider<List<ExerciseCatalogItem>>((ref) async {
-  final data = await ref.read(catalogApiProvider).fetchExercises();
+  final data =
+  await ref.read(catalogApiProvider).fetchExercises();
 
-  final items =
-  data.map((e) => ExerciseCatalogItem.fromJson(e)).toList()
+  return data
+      .map((e) => ExerciseCatalogItem.fromJson(e))
+      .toList()
     ..sort((a, b) =>
         a.name.toLowerCase().compareTo(b.name.toLowerCase()));
-
-  return items;
 });
