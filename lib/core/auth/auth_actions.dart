08bbfed7b87ff37a1eba57a861bf1a97ab3ca313
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 16:55:18 +0200

    Refactor auth_actions

--- lib/core/auth/auth_actions.dart
+++ lib/core/auth/auth_actions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'auth_provider.dart';
+import 'auth_state_provider.dart';
 
 class AuthActions {
   static void logout(WidgetRef ref, BuildContext context) {
