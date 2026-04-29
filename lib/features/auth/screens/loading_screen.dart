dd4919ef2134153200307c92eb19d511304a8c00
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 16:55:20 +0200

    Refactor loading_screen UI

--- lib/features/auth/screens/loading_screen.dart
+++ lib/features/auth/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../../../../core/auth/auth_provider.dart';
+import '../../../../core/auth/auth_state_provider.dart';
 
 class LoadingScreen extends ConsumerStatefulWidget {
   const LoadingScreen({super.key});
