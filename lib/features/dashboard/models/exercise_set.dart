bf1e34a9bcfe6afdcdb7d8e4cb8f7fd4131b95db
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 13:58:21 +0200

    exercise set model with weight, reps, json and copyWith

--- lib/features/dashboard/models/exercise_set.dart
+++ lib/features/dashboard/models/exercise_set.dart
@@ -2,28 +2,17 @@ class ExerciseSet {
   final double weight;
   final int reps;
 
-  const ExerciseSet({
-    required this.weight,
-    required this.reps,
-  });
+  const ExerciseSet({required this.weight, required this.reps});
 
-  ExerciseSet copyWith({
-    double? weight,
-    int? reps,
-  }) =>
-      ExerciseSet(
-        weight: weight ?? this.weight,
-        reps: reps ?? this.reps,
-      );
+  ExerciseSet copyWith({double? weight, int? reps}) => ExerciseSet(
+    weight: weight ?? this.weight,
+    reps: reps ?? this.reps,
+  );
 
-  factory ExerciseSet.fromJson(Map<String, dynamic> json) =>
-      ExerciseSet(
-        weight: (json['weight'] as num?)?.toDouble() ?? 0,
-        reps: (json['repetitions'] as num?)?.toInt() ?? 0,
-      );
+  factory ExerciseSet.fromJson(Map<String, dynamic> json) => ExerciseSet(
+    weight: (json['weight'] as num?)?.toDouble() ?? 0,
+    reps: (json['repetitions'] as num?)?.toInt() ?? 0,
+  );
 
-  Map<String, dynamic> toJson() => {
-    'weight': weight,
-    'repetitions': reps,
-  };
+  Map<String, dynamic> toJson() => {'weight': weight, 'repetitions': reps};
 }
