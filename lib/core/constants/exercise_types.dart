75ac0810463943f2a15d694ea69344f563efb470
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 13:58:22 +0200

    define exercise type constants for catalog filters

---
+++ lib/core/constants/exercise_types.dart
@@ -0,0 +1,13 @@
+class ExerciseTypes {
+  static const ALL = "ALL";
+  static const STRENGTH = "STRENGTH";
+  static const CARDIO = "CARDIO";
+  static const MOBILITY = "MOBILITY";
+
+  static const all = [
+    ALL,
+    STRENGTH,
+    CARDIO,
+    MOBILITY,
+  ];
+}
