a8986137453c135ac96b75af6328d2d00356293f
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 16:55:19 +0200

    Update exercise_service_provider state

--- lib/features/dashboard/state/exercise_service_provider.dart
+++ lib/features/dashboard/state/exercise_service_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/api/exercise_service.dart';
+import '../../../core/api/catalog_exercise_service.dart';
 import '../../../core/network/dio_provider.dart';
 
 final exerciseServiceProvider = Provider<ExerciseService>((ref) {
