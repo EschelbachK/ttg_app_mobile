3e2260bb4fc6527051c208d5be46e53f94526bbd
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 13:58:21 +0200

    add exercise service for create, update, set, ranked alternatives

---
+++ lib/core/api/exercise_service.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+import '../../features/dashboard/models/ranked_exercise_response.dart';
+import '../../features/dashboard/models/training_exercise.dart';
+import '../error/global_error_handler.dart';
+
+class ExerciseService {
+  final Dio dio;
+  ExerciseService(this.dio);
+
+  Future<TrainingExercise> createExercise(Map<String, dynamic> data) async {
+    try {
+      final res = await dio.post('/api/exercise/create', data: data);
+      return TrainingExercise.fromJson(res.data);
+    } catch (e) {
+      throw GlobalErrorHandler.handle(e);
+    }
+  }
+
+  Future<TrainingExercise> updateExercise(String id, Map<String, dynamic> data) async {
+    try {
+      final res = await dio.put('/api/exercise/update/$id', data: data);
+      return TrainingExercise.fromJson(res.data);
+    } catch (e) {
+      throw GlobalErrorHandler.handle(e);
+    }
+  }
+
+  Future<void> setExercise(String id, Map<String, dynamic> setEntry) async {
+    try {
+      await dio.put('/api/exercise/set/$id', data: setEntry);
+    } catch (e) {
+      throw GlobalErrorHandler.handle(e);
+    }
+  }
+
+  Future<List<RankedExerciseResponse>> getRankedAlternatives(String exerciseId) async {
+    try {
+      final res = await dio.get('/api/exercise/ranked/$exerciseId');
+      final list = List<Map<String, dynamic>>.from(res.data as List);
+      return list.map((e) => RankedExerciseResponse.fromJson(e)).toList();
+    } catch (e) {
+      throw GlobalErrorHandler.handle(e);
+    }
+  }
+}
