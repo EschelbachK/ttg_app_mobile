63e6959082a333604b246dc96cfa246eb0601de1
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 13:58:21 +0200

    define exercise tags constants

---
+++ lib/core/constants/exercise_tags.dart
@@ -0,0 +1,13 @@
+class ExerciseTags {
+  static const WARMUP = "Warmup";
+  static const COOLDOWN = "Cooldown";
+  static const WEIGHTED = "Weighted";
+  static const BODYWEIGHT = "Bodyweight";
+
+  static const all = [
+    WARMUP,
+    COOLDOWN,
+    WEIGHTED,
+    BODYWEIGHT,
+  ];
+}
