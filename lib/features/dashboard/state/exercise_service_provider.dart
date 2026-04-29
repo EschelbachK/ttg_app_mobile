560bdc4d77bf6c0886ad14120145630e2578d482
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-29 13:58:21 +0200

    provider for exercise service

---
+++ lib/features/dashboard/state/exercise_service_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/api/exercise_service.dart';
+import '../../../core/network/dio_provider.dart';
+
+final exerciseServiceProvider = Provider<ExerciseService>((ref) {
+  return ExerciseService(ref.read(dioProvider));
+});
